afdfda63422689f8e8cbb810525dd566b67f9693
belangeo <belangeo@f71ddfb8-18aa-11de-896d-b39e94f1936c>
2009-03-25 16:16:29 +0000

    beginning port to Windows

--- SoundGrain.py
+++ SoundGrain.py
@@ -786,9 +786,9 @@ class DrawingSurface(wx.Panel):
         for i in range(chnls):
             monofile = os.path.join(os.path.expanduser('~'), os.path.split(file)[1].rsplit('.',1)[0] + '-' + str(i) + '.aif')
             if systemPlatform == 'darwin':    
-                cspipe3 = Popen('/usr/local/bin/csound -U envext -o "%s/anal%s" -w .001 ' % (os.path.expanduser('~'),i) + monofile, shell=True, stdin=PIPE)
+                cspipe3 = Popen('/usr/local/bin/csound -U envext -o "%s/anal%s" -w .001 "%s"' % (os.path.expanduser('~'),i,monofile), shell=True, stdin=PIPE)
             elif systemPlatform == 'win32':
-                cspipe3 = Popen('start /REALTIME /WAIT csound -U envext -o "%s/anal%s" -w .001 ' % (os.path.expanduser('~'),i) + monofile, shell=True, stdin=PIPE)
+                cspipe3 = Popen('start /REALTIME /WAIT csound -U envext -o "%s/anal%s" -w .001 "%s"' % (os.path.expanduser('~'),i,monofile), shell=True, stdin=PIPE)
             cspipe3.wait()  
             os.remove(os.path.join(os.path.expanduser('~'), monofile))      
         self.make_list()        
