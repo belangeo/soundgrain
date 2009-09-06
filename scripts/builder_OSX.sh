e38efd19def0d6a1ea02f9c808982c08ada1572b
belangeo <belangeo@f71ddfb8-18aa-11de-896d-b39e94f1936c>
2009-09-06 01:32:01 +0000

    fine tunnig version 2.0 OS X packaging

--- scripts/builder_OSX.sh
+++ scripts/builder_OSX.sh
@@ -3,13 +3,14 @@ py2applet --make-setup SoundGrain.py Resources/*
 python2.5 setup.py py2app
 rm -f setup.py
 rm -rf build
-#cp scripts/README.txt dist/
-mv dist SoundGrain_v1.0
-cd SoundGrain_v1.0
+cp scripts/README.txt dist/
+mv dist SoundGrain_v2.0
+cd SoundGrain_v2.0
 find . -name .svn -depth -exec rm -rf {} \;
 find . -name *.pyc -depth -exec rm -f {} \;
 find . -name .* -depth -exec rm -f {} \;
+mkdir SoundGrain.app/Contents/Resources/psyco
+cp /Library/Frameworks/Python.framework/Versions/2.5/lib/Python2.5/site-packages/psyco/* SoundGrain.app/Contents/Resources/psyco
 cd ..
-tar -cjvf SoundGrain_v1.0.tar.bz2 SoundGrain_v1.0
-#mv SoundGrain/SoundGrain.app SoundGrain.app
-rm -rf SoundGrain_v1.0
+tar -cjvf SoundGrain_v2.0.tar.bz2 SoundGrain_v2.0
+rm -rf SoundGrain_v2.0
