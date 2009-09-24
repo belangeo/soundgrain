60836a62d173c3ca1dd20ffb27df95a28ac95bd7
belangeo <belangeo@f71ddfb8-18aa-11de-896d-b39e94f1936c>
2009-09-24 02:42:49 +0000

    windows font

--- Resources/Slider.py
+++ Resources/Slider.py
@@ -318,7 +318,7 @@ class ControlSlider(wx.Panel):
             dc.DrawRoundedRectangleRect(rec2, 3)
 
         if sys.platform in ['win32', 'linux2']:
-            dc.SetFont(wx.Font(8, wx.ROMAN, wx.NORMAL, wx.NORMAL))
+            dc.SetFont(wx.Font(7, wx.ROMAN, wx.NORMAL, wx.NORMAL))
         else:    
             dc.SetFont(wx.Font(10, wx.ROMAN, wx.NORMAL, wx.NORMAL))
 
