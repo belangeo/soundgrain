387cb605ef516066ed4eb17f008509784a8129c3
belangeo <belangeo@f71ddfb8-18aa-11de-896d-b39e94f1936c>
2011-01-25 02:35:05 +0000

    Icon png files

--- trunk/scripts/builder_OSX.sh
+++ trunk/scripts/builder_OSX.sh
@@ -3,15 +3,15 @@ py2applet --make-setup SoundGrain.py Resources/*
 python setup.py py2app --plist=scripts/info.plist
 rm -f setup.py
 rm -rf build
-mv dist SoundGrain_v3.0
-cd SoundGrain_v3.0
+mv dist SoundGrain_v4.0
+cd SoundGrain_v4.0
 find . -name .svn -depth -exec rm -rf {} \;
 find . -name *.pyc -depth -exec rm -f {} \;
 find . -name .* -depth -exec rm -f {} \;
 mkdir SoundGrain.app/Contents/Resources/psyco
 cp /Library/Frameworks/Python.framework/Versions/2.6/lib/Python2.6/site-packages/psyco/* SoundGrain.app/Contents/Resources/psyco
 cd ..
-cp -R SoundGrain_v3.0/SoundGrain.app .
-tar -cjvf SoundGrain_v3.0.1.tar.bz2 SoundGrain.app
-rm -rf SoundGrain_v3.0
-rm -rf SoundGrain.app
+cp -R SoundGrain_v4.0/SoundGrain.app .
+#tar -cjvf SoundGrain_v4.0.tar.bz2 SoundGrain.app
+rm -rf SoundGrain_v4.0
+#rm -rf SoundGrain.app
