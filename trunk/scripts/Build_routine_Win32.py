ac981488e7532a527baea8885159b15c35e8987c
belangeo <belangeo@f71ddfb8-18aa-11de-896d-b39e94f1936c>
2015-06-27 22:52:29 +0000

    Updated windows builder script.

--- trunk/scripts/Build_routine_Win32.py
+++ trunk/scripts/Build_routine_Win32.py
@@ -5,8 +5,8 @@ version = sys.version_info[:2]
 os.system('C:\Python%d%d\Scripts\pyi-makespec -F -c --icon=Resources\SoundGrain.ico "SoundGrain.py"' % version)
 os.system('C:\Python%d%d\Scripts\pyi-build "SoundGrain.spec"' % version)
 
-os.system("python ..\pyinstaller\Configure.py")
-os.system('python ..\pyinstaller\Makespec.py -F -c --icon=Resources\SoundGrain.ico "SoundGrain.py"')
+#os.system("python ..\pyinstaller\Configure.py")
+#os.system('python ..\pyinstaller\Makespec.py -F -c --icon=Resources\SoundGrain.ico "SoundGrain.py"')
 
 os.system("svn export . SoundGrain_Win")
 os.system("copy dist\SoundGrain.exe SoundGrain_Win /Y")
